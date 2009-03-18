2879b335f953b53035d18fa8a5266ea7ad184ee1
killersoft <killersoft@197917fa-363a-0410-b49c-77c20b5a621a>
2009-03-18 02:30:28 +0000

    moved

--- trunk/ext/ical/ical_ce.h
+++
@@ -1,9 +0,0 @@
-
-/* All iCal extension zend_class_entry entries defined here */
-
-#ifndef ICAL_CE_H
-#define ICAL_CE_H
-
-extern zend_class_entry *ical_component_class_entry;
-
-#endif /* ICAL_CE_H */
